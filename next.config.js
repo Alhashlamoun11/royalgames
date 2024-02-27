eeaa1476fb49a984911e306af1a5f47b898be9e4
Alhashlamoun11 <144154049+Alhashlamoun11@users.noreply.github.com>
2024-02-27 23:58:46 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
         ignoreBuildErrors: true,
       },
       env: {
-        BACKEND_URL: 'http://localhost:8080/api',
+        BACKEND_URL: 'http://173.252.167.140:8080/api',
         NEXT_PUBLIC_API_URL: 'your_api_url',
       },
   
